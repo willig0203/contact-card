fec8b329cbd75d398d4b5ec0b69c59aadd75c961
willig0203 <100536076+willig0203@users.noreply.github.com>
2022-08-27 08:44:49 -0500

    added my logo

---
+++ client/src/index.js
@@ -0,0 +1,12 @@
+import "./js/form";
+import "./js/submit";
+
+import Logo from "./images/logo02.jpeg";
+import Bear from "./images/bear.png";
+import Dog from "./images/dog.png";
+
+window.addEventListener("load", function () {
+  document.getElementById("logo").src = Logo;
+  document.getElementById("bearThumbnail").src = Bear;
+  document.getElementById("dogThumbnail").src = Dog;
+});
