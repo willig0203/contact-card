a32fc92beaa3967dfc8279bdd8fada69267ec10d
willig0203 <100536076+willig0203@users.noreply.github.com>
2022-08-27 12:24:33 -0500

    Add to staging Lesson 2

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import "./css/index.css";
 import { Tooltip, Toast, Popover } from "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-import Logo from "./images/logo02.jpeg";
+import Logo from "./images/logo.png";
 import Bear from "./images/bear.png";
 import Dog from "./images/dog.png";
 
