6fa71c3669877518c0fbd0adc5bff5a3f4319c02
willig0203 <100536076+willig0203@users.noreply.github.com>
2022-08-28 00:04:38 -0500

    database operations get and put complete

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import "./js/index.js";
 import "./css/index.css";
 
-import { initdb } from "./js/database";
+import { initdb, getDb, postDb } from "./js/database";
 
 import { Tooltip, Toast, Popover } from "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -12,6 +12,10 @@ import Dog from "./images/dog.png";
 
 window.addEventListener("load", function () {
   initdb();
+  // // We are temporarily placing getDb() and postDb() function calls here for testing. We will move it to another event listener later.
+  // getDb();
+  // postDb("Lernantino", "learnantino@test.com", 8186601234, "Bear");
+  // getDb();
   document.getElementById("logo").src = Logo;
   document.getElementById("bearThumbnail").src = Bear;
   document.getElementById("dogThumbnail").src = Dog;
--- client/src/js/database.js
+++ client/src/js/database.js
@@ -17,3 +17,52 @@ export const initdb = async () => {
     },
   });
 };
+
+// Export a function we will use to GET to the database.
+export const getDb = async () => {
+  console.log("GET from the database");
+
+  // Create a connection to the IndexedDB database and
+  // the version we want to use.
+  const contactDb = await openDB("contact_db", 1);
+
+  // Create a new transaction and specify the store and data privileges.
+  const tx = contactDb.transaction("contacts", "readonly");
+
+  // Open up the desired object store.
+  const store = tx.objectStore("contacts");
+
+  // Use the .getAll() method to get all data in the database.
+  const request = store.getAll();
+
+  // Get confirmation of the request.
+  const result = await request;
+  console.log("result.value", result);
+  return result;
+};
+
+// Export a function we will use to POST to the database.
+export const postDb = async (name, email, phone, profile) => {
+  console.log("POST to the database");
+
+  // Create a connection to the database and specify the version we want to use.
+  const contactDb = await openDB("contact_db", 1);
+
+  // Create a new transaction and specify the store and data privileges.
+  const tx = contactDb.transaction("contacts", "readwrite");
+
+  // Open up the desired object store.
+  const store = tx.objectStore("contacts");
+
+  // Use the .add() method on the store and pass in the content.
+  const request = store.add({
+    name: name,
+    email: email,
+    phone: phone,
+    profile: profile,
+  });
+
+  // Get confirmation of the request.
+  const result = await request;
+  console.log("🚀 - data saved to the database", result);
+};
