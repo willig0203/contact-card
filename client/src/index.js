9e1222c9ece1969e2d55cb5fd78f22f5bfe57b06
willig0203 <100536076+willig0203@users.noreply.github.com>
2022-08-27 23:20:14 -0500

    contacts database created

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,17 @@
 import "./js/index.js";
 import "./css/index.css";
 
+import { initdb } from "./js/database";
+
 import { Tooltip, Toast, Popover } from "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-import Logo from "./images/logo02.jpeg";
+import Logo from "./images/logo.png";
 import Bear from "./images/bear.png";
 import Dog from "./images/dog.png";
 
 window.addEventListener("load", function () {
+  initdb();
   document.getElementById("logo").src = Logo;
   document.getElementById("bearThumbnail").src = Bear;
   document.getElementById("dogThumbnail").src = Dog;
