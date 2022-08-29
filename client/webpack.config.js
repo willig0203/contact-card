d45e7be24f526bbef376a97e1859734ccc44d42e
willig0203 <100536076+willig0203@users.noreply.github.com>
2022-08-29 15:34:46 -0500

    injectmanifest

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const WorkboxPlugin = require("workbox-webpack-plugin");
+const { InjectManifest } = require("workbox-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
@@ -36,30 +37,9 @@ module.exports = {
       title: "Webpack Plugin",
     }),
 
-    new WorkboxPlugin.GenerateSW({
-      // Do not precache images
-      exclude: [/\.(?:png|jpg|jpeg|svg)$/],
-
-      // Define runtime caching rules.
-      runtimeCaching: [
-        {
-          // Match any request that ends with .png, .jpg, .jpeg or .svg.
-          urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
-
-          // Apply a cache-first strategy.
-          handler: "CacheFirst",
-
-          options: {
-            // Use a custom cache name.
-            cacheName: "images",
-
-            // Only cache 1 images
-            expiration: {
-              maxEntries: 1,
-            },
-          },
-        },
-      ],
+    new InjectManifest({
+      swSrc: "./src/sw.js",
+      swDest: "service-worker.js",
     }),
   ],
 };
